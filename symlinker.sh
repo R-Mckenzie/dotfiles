4d328981594496f1ce96d70ca33d59183640b2a9
Ross Mckenzie <rossmckenzie11@gmail.com>
2022-04-17 17:39:17 +0100

    add nvim config to dotfiles

--- symlinker.sh
+++ symlinker.sh
@@ -13,19 +13,27 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 
 # list of files/folders to symlink in homedir
-files="gitconfig"
+home_files="gitconfig"
+config_files="nvim"
 
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo -n "Creating $olddir for backup of any existing dotfiles"
 mkdir -p $olddir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
+for file in $home_files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+done
+
+for file in $config_files; do
+    echo "Moving any existing dotfiles from ~/.config to $olddir"
+    mv ~/.config/$file $olddir
+    echo "Creating symlink to $file in config folder."
+    ln -s $dir/$file ~/.config/$file
 done
