e30e338421a6d26204d77ca02e6e98c4949783a2
Ross Mckenzie <rossmckenzie11@gmail.com>
2022-04-22 14:52:15 +0100

    add suckless src

--- symlinker.sh
+++ symlinker.sh
@@ -15,6 +15,7 @@ olddir=~/dotfiles_old
 # list of files/folders to symlink in homedir
 home_files="gitconfig"
 config_files="nvim"
+suckless_src_files="dwm st dmenu slstatus"
 
 ##########
 
@@ -24,16 +25,26 @@ mkdir -p $olddir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $home_files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+echo "done"
 
+echo "Moving any existing config files from ~/.config to $olddir"
 for file in $config_files; do
-    echo "Moving any existing dotfiles from ~/.config to $olddir"
     mv ~/.config/$file $olddir
-    echo "Creating symlink to $file in config folder."
+    echo "Creating symlink to $file in .local/src folder."
     ln -s $dir/$file ~/.config/$file
 done
+echo "done"
+
+echo "Moving any existing src files from ~/.local/src to $olddir"
+for file in $suckless_src_files; do
+    mv ~/.local/src/$file $olddir
+    echo "Creating symlink to $file in config folder."
+    ln -s $dir/src/$file ~/.local/src/$file
+done
+echo "done"
