c52ce8a4d95402f9acc346c4ae27cc6a4ff7b328
Ross Mckenzie <rossmckenzie11@gmail.com>
2023-12-24 00:46:19 +0000

    update nvim

--- nvim/lua/go.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_exec([[autocmd BufWritePre *.go lua vim.lsp.buf.formatting()]], false)
-
-vim.api.nvim_set_keymap('n', '<leader>gt', ':wall <bar> !go test<CR>', { noremap = true })
--- nvim/lua/statusline.lua
+++
@@ -1,8 +0,0 @@
-local o = vim.o
-
-o.statusline = ""
-o.statusline = o.statusline .. "%f "
-                            .. "%y |"
-                            .. "%="
-                            .. "| [%l,%c]  "
-                            .. "%p%% "
